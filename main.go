98153af3f7dec163385d530c2d8154730ae837d2
Vinícius Soares <vinisoares15@gmail.com>
2023-04-12 20:51:40 -0300

    feat: add sub package

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "firstproject/API/router"
 
 func main() {
-	// print hello world
-	println("Hello World")
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
 
+	//initialize router
+	router.Initialize()
 }
